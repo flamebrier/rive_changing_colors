a4468762762e56711b1b938f8f428564ca15745e
flamebrier <flamebrier@gmail.com>
2021-06-27 18:50:30 +0300

    Prerequisites

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:rive_changing_colors/theming.dart';
+import 'package:rive/rive.dart';
+
+import 'chat_icon_controller.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: mainTheme,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Rive Colors Changing Example"),
+      ),
+      body: Center(
+        child: Container(
+          height: 48,
+          child: RiveAnimation.asset(
+            "flare/message-icon.riv",
+            animations: ["Hover_loop"],
+            controllers: [ChatIconController("Hover_loop")],
+          ),
+        )
+      )
+    );
+  }
+}
---
+++ lib/theming.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final ThemeData mainTheme = ThemeData(
+  primarySwatch: Colors.amber,
+  brightness: Brightness.dark
+);
